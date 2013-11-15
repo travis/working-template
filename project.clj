80a447b7e718fdceead2cc647b40ec2b1cb93bcb
Travis Vachon <travis@copious.com>
2013-11-15 15:14:19 -0500

    Add url, description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject working-template/lein-template "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A working leiningen template, for testing"
+  :url "https://github.com/travis/working-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
